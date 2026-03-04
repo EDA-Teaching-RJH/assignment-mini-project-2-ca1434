a978a51a14474f9e20c7acb374f683ef6497fd8b
Chloe Augustus <ca14@kent.ac.uk>
2026-03-04 21:25:17 +0000

    This code askes for users id and name and returns  a welome message

--- Mathstest.py
+++ Mathstest.py
@@ -5,6 +5,11 @@ class questions:
     def __init__(self,name,id):
         self.name=name 
         self.id=id
+
+    def student(self):
+        self.n = input("name")
+        self.i = input("Id")
+        print("hello",self.n)
   
     
 
