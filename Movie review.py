78d0d07ca07185d00e2cb770fa715248bd496aec
Chloe Augustus <ca14@kent.ac.uk>
2026-03-10 20:30:08 +0000

    def review lets the film name rating and opinion be inputted and makes sure the rating is the  correct number

--- Movie review.py
+++ Movie review.py
@@ -4,5 +4,14 @@ class questions:
     def __init__(self,name)
         self.name=name
         self.Mscore=0
+
+    def people(self):
+        print("hello",self.name)
+    
+    def review(self):
+        film = input("enter movie you want to review:  ")
+        rating = input ("enter a rating between 1 and 10")
+        opinion = input ("enter your opinion about the movie:")
+
         
         
