f83acc8a182bb1e5c8717c5434ff26c0cc553c51
Chloe Augustus <ca14@kent.ac.uk>
2026-03-10 20:09:27 +0000

    m

--- Mathstest.py
+++ Mathstest.py
@@ -5,18 +5,16 @@ class questions:
     def __init__(self,name,id):
         self.name=name 
         self.id=id
-        self.score=0
+        self.Mscore=0
 
-    def student(self):
-
-        print("hello",self.name)
+    def people(self):
+      print("hello",self.name)
   
-    def test(self):
-       a = random.randint(1,20)
-       b = random.randint(1,20)
-       c = a+b
+    def review(self):
+       film = input("enter which movie to review: Iron man 1 , Captain America: cival war , avengers endgame:")
+       rating = input ("enter a score from 1 to 10:")
+       opinion = input ("enter your opinion of the movie:")
 
-       answer = int(input (f"whats {a} + {b}?"))
        if answer==c:
            print("correct")
            self.score += 1
@@ -25,32 +23,40 @@ class questions:
 
     def save(self):
         with open ("scores.txt","a") as f:
-            f.write(f"student: {self.name} id: {self.id} score: {self.score}/n")
+            f.write(f"student: {self.name} id: {self.id} score: {self.score}")
     
     def history(self):
         with open ("scores.txt","r") as f:
          print(f.read())
+    
+    def delete(self):
+        with open("scores.txt","w") as f:
+            pass
+            print("deleted")
         
 name = input("name")
 id = input("Id")
 
 maths = questions(name,id)
 maths.student()
-maths.test()
 maths.save()
-maths.history()
 
-while true:
-    print("1.test 2.view test history 3.delete history 4.quit")
+while True:
+    print("1.test 2.new student 3.view test history 4.delete history 5.quit")
     choice = input("choose option")
 
-    if choice == 1:
+    if choice == "1":
       maths.test()
-    elif choice == 2:
+      
+    elif choice == "2":
+        name = input("name")
+        id = input("Id")
+        maths.student()
+    elif choice == "3":
         maths.history()
-    elif choice == 3:
-        maths.delete
-    elif choice == 4:
+    elif choice == "4":
+        maths.delete()
+    elif choice == "5":
         print("goodbye")
         break
     else:
---
+++ Movie review.py
@@ -0,0 +1,8 @@
+import random 
+
+class questions:
+    def __init__(self,name)
+        self.name=name
+        self.Mscore=0
+        
+        
