aa1ca1d4db2c99fd8e2044857698e92fab0a8d50
Chloe Augustus <ca14@kent.ac.uk>
2026-03-21 22:00:13 +0000

    I added functions to test the regex of the opinion to make sure it rejects empty imputs and only number inputs .

--- test_movie.py
+++ test_movie.py
@@ -8,10 +8,19 @@ class testquestions(unittest.TestCase):
         y="er45"
         assert re.search(r"^[a-zA-Z]{2}[0-9]{2}",x)
         assert re.search(r"^[a-zA-Z]{2}[0-9]{2}",y)
-    def test_nuser(self):
-        x="gpn45"
-        y="23ty"
-        assert not re.search(r"^[a-zA-Z]{2}[0-9]{2}",x)
-        assert not re.search(r"^[a-zA-Z]{2}[0-9]{2}",y)
+    def test_notuser(self):
+        a="gpn45"
+        b="23ty"
+        assert not re.search(r"^[a-zA-Z]{2}[0-9]{2}",a)
+        assert not re.search(r"^[a-zA-Z]{2}[0-9]{2}",b)
 
     def test_opinion(self):
+        c="Amazing"
+        d="ok"
+        assert re.search(r"^[a-zA-Z]",c)
+        assert re.search(r"^[a-zA-Z]",d)
+    def test_notopinion(self):
+        e="12345567"
+        f=""
+        assert not re.search(r"^[a-zA-Z]",e)
+        assert not re.search(r"^[a-zA-Z]",f)
