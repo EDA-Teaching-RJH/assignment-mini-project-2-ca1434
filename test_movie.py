0f140cff8f8b7101814a6ddbdbbefc067bc98bed
Chloe Augustus <ca14@kent.ac.uk>
2026-03-22 17:43:37 +0000

    1) I imported unittest and chnage asses to self.assestTrue so that the code can carry out the correct test 2)I added a function for the ratings to test numbers within and outside the test range

--- test_movie.py
+++ test_movie.py
@@ -1,4 +1,5 @@
 import re 
+import unittest 
 from movie import questions
 
 class testquestions(unittest.TestCase):
@@ -6,21 +7,33 @@ class testquestions(unittest.TestCase):
     def test_user(self): 
         x="qw23"
         y="er45"
-        assert re.search(r"^[a-zA-Z]{2}[0-9]{2}",x)
-        assert re.search(r"^[a-zA-Z]{2}[0-9]{2}",y)
+        self.assertTrue (re.search(r"^[a-zA-Z]{2}[0-9]{2}",x))
+        self.assertTrue(re.search(r"^[a-zA-Z]{2}[0-9]{2}",y))
     def test_notuser(self):
         a="gpn45"
         b="23ty"
-        assert not re.search(r"^[a-zA-Z]{2}[0-9]{2}",a)
-        assert not re.search(r"^[a-zA-Z]{2}[0-9]{2}",b)
+        self.assertFalse( re.search(r"^[a-zA-Z]{2}[0-9]{2}",a))
+        self.assertFalse( re.search(r"^[a-zA-Z]{2}[0-9]{2}",b))
 
     def test_opinion(self):
         c="Amazing"
         d="ok"
-        assert re.search(r"^[a-zA-Z]",c)
-        assert re.search(r"^[a-zA-Z]",d)
+        self.assertTrue (re.search(r"^[a-zA-Z]",c))
+        self.assertTrue( re.search(r"^[a-zA-Z]",d))
     def test_notopinion(self):
         e="12345567"
         f=""
-        assert not re.search(r"^[a-zA-Z]",e)
-        assert not re.search(r"^[a-zA-Z]",f)
+        self.assertFalse( re.search(r"^[a-zA-Z]",e))
+        self.assertFalse( re.search(r"^[a-zA-Z]",f))
+    
+    def test_rating(self):
+        g="8"
+        h="2"
+        self.assertTrue( 1<=g<=10)
+        self.assertTrue( 1<=h<=10)
+
+    def test_notrating(self):
+        i="15"
+        j="0"
+        self.assertTrue( 1<=i<=10)
+        self.assertTrue( 1<=j<=10)
